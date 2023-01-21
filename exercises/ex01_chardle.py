792cd5b64448f84fa4de7292d82c9df62d4c3c83
Hari Potharaju <hpot@unc.edu>
2023-01-21 17:09:33 -0500

    progress on ex. 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,62 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = 730575328
+
+word:str = input("Enter a 5-character word: ")
+
+if len(word) < 5:
+    exit("Error: Word must contain 5 characters")
+
+if len(word) > 5:
+   exit("Error: Word must contain 5 characters")
+
+
+
+letter: str = input("Enter a single character: ")
+
+if len(letter) != 1:
+    exit("Error: Character must be a single character.")
+
+x: int = 0
+
+
+
+print ("Searching for " + letter + " in " + word)
+
+if (word [0]) == letter:
+    print( word[0] + " found at index 0")
+if (word [1]) == letter:
+    print( word[1] + " found at index 1")
+if (word [2]) == letter:
+    print( word[2] + " found at index 2")
+if (word [3]) == letter:
+    print( word[3] + " found at index 3")   
+if (word [4]) == letter:
+    print( word[4] + " found at index 4") 
+
+  
+
+if (word[0]) == letter:
+    x = x + 1
+if (word[1]) == letter:
+   x = x + 1
+if (word[2]) == letter:
+   x = x + 1
+if (word[3]) == letter:
+    x = x + 1
+if (word[4]) == letter:
+    x = x + 1
+if x == 0:
+    x = str("No")
+
+print(x,"instances of", letter, "found in", word)
+
+
+
+
+
+
+
+ 
+
+
--- lessons/input.py
+++ lessons/input.py
@@ -1,7 +1,6 @@
-"""My first program for COMP110."""
+"""If its raining, pack an umbrella"""
 
-__author__ = "730575328"
-print("Hello, world. Slay")
 weather: str = input("What is the weather like?")
+
 if (weather == "rain"):
     print("pack an umbrella queen!")
