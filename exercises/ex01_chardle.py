4a1f53490b674f3efb596e273b3d89cc382550a8
Hari Potharaju <hpot@unc.edu>
2023-01-22 21:18:40 -0500

    final

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,31 +4,29 @@ __author__ = "730575328"
 
 word: str = input("Enter a 5-character word: ")
 
-if len(word) < 5:
-    exit("Error: Word must contain 5 characters")
+if len(word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
 
-if len(word) > 5:
-    exit("Error: Word must contain 5 characters")
 letter: str = input("Enter a single character: ")
 
 if len(letter) != 1:
-    exit("Error: Character must be a single character.")
+    print("Error: Character must be a single character.")
+    exit()
 
 x: int = 0
-print ("Searching for " + letter + " in " + word)
+print("Searching for " + letter + " in " + word)
 
-if (word [0]) == letter:
-    print( word [0] + " found at index 0")
-if (word [1]) == letter:
-    print( word [1] + " found at index 1")
-if (word [2]) == letter:
-    print( word [2] + " found at index 2")
-if (word [3]) == letter:
-    print( word [3] + " found at index 3")   
-if (word [4]) == letter:
-    print( word [4] + " found at index 4") 
-
-  
+if (word[0]) == letter:
+    print(word[0] + " found at index 0")
+if (word[1]) == letter:
+    print(word[1] + " found at index 1")
+if (word[2]) == letter:
+    print(word[2] + " found at index 2")
+if (word[3]) == letter:
+    print(word[3] + " found at index 3")   
+if (word[4]) == letter:
+    print(word[4] + " found at index 4") 
 
 if (word[0]) == letter:
     x = x + 1
@@ -41,9 +39,9 @@ if (word[3]) == letter:
 if (word[4]) == letter:
     x = x + 1
 if x == 0:
-    x = str("No")
+    print("No")
 
 if x != 1:
     print(x,"instances of", letter, "found in", word)
 else:
-    print(x ,"instance of", letter , "found in" , word)
+    print(x,"instance of", letter, "found in", word)    
