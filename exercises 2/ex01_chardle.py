b51c94432a7bda5ef9304b9324f0cd63cb9559d7
Reem Fayyad <rmfayyad@unc.edu>
2023-01-23 19:16:21 -0500

    change

--- exercises 2/ex01_chardle.py
+++ exercises 2/ex01_chardle.py
@@ -2,22 +2,20 @@
 
 __author__ = "730558596"
 
-user_input: str = input("Enter a 5-character word: ")
+user_input: str = input("Enter 5-character word: ")
 if len(user_input) != 5:
-    print(" Error: Word must contain 5 characters ")
+    print(" Error: Word must contain 5 characters.")
     quit()
-
 char_val = input("Enter a single character: ")
-print(" Searching for " + char_val + " in " + user_input) 
+print(" Seraching for " + char_val + " in " + user_input) 
 if len(char_val) != 1:
     print("Error: Character must be a single character. ")
     quit()
-
 count = 0
 
 if user_input[0] == char_val:
     print(char_val + " found at index 0 ")
-    count += 1
+    count += 1 
 if user_input[1] == char_val:
     print(char_val + " found at index 1 ")
     count += 1
@@ -30,6 +28,4 @@ if user_input[3] == char_val:
 if user_input[4] == char_val:
     print(char_val + " found at index 4 ")
     count += 1 
-print(str(count) + " instances of " + char_val + " found in " + user_input)
-
-print(" No instances of " + char_val + "in" + user_input)
+print(str(count) + " instance of " + char_val + " found in " + user_input)
