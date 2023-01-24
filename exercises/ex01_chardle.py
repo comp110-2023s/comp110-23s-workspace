897323427f38e5add0074e66f5f9074c80720c6e
Reem Fayyad <rmfayyad@unc.edu>
2023-01-23 22:37:34 -0500

    changes

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -13,27 +13,27 @@ if len(char_val) != 1:
     print("Error: Character must be a single character. ")
     exit()
 
-count = 0
+match_count = 0
 
 if user_input[0] == char_val:
     print(char_val + " found at index 0 ")
-    count += 1
+    match_count += 1
 if user_input[1] == char_val:
     print(char_val + " found at index 1 ")
-    count += 1
+    match_count += 1
 if user_input[2] == char_val:
     print(char_val + " found at index 2 ")
-    count += 1
+    match_count += 1
 if user_input[3] == char_val:
     print(char_val + " found at index 3 ")
-    count += 1
+    match_count += 1
 if user_input[4] == char_val:
     print(char_val + " found at index 4 ")
-    count += 1 
+    match_count += 1 
 
-if count > 1:
-    print(str(count) + " instances of " + char_val + " found in " + user_input)
-if count == 1:
-     print(str(count) + " instance of " + char_val + " found in " + user_input)
+if match_count > 1:
+    print(str(match_count) + " instances of " + char_val + " found in " + user_input)
+if match_count == 1:
+     print(str(match_count) + " instance of " + char_val + " found in " + user_input)
 else:
     print(" No instnaces of " + char_val + " found in " + user_input)
