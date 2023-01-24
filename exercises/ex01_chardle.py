460d0b556ceb0a6924d4bc2006b02537eebdad45
Reem Fayyad <rmfayyad@unc.edu>
2023-01-23 23:04:23 -0500

    changes

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,13 +4,13 @@ __author__ = "730558596"
 
 user_input: str = input("Enter a 5-character word: ")
 if len(user_input) != 5:
-    print("Error: Word must contain 5 characters ")
+    print("Error: Word must contain 5 characters")
     exit()
 
 char_val = input("Enter a single character: ")
 print("Searching for " + char_val + " in " + user_input) 
 if len(char_val) != 1:
-    print("Error: Character must be a single character. ")
+    print("Error: Character must be a single character.")
     exit()
 
 match_count = 0
