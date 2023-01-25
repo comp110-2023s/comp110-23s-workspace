84758d5f81a2ec40977c0a44930a932bc971b922
Isaac Mink <imink@unc.edu>
2023-01-24 22:56:32 -0500

    ex01Final

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,37 +3,37 @@
 __author__ = "730466092"
 
 five_cha_word: str = input("Enter a 5-character word: ")
-if(len(five_cha_word) != 5):
-    print("Word must contain 5 characters")
+if (len(five_cha_word) != 5):
+    print("Error: Word must contain 5 characters")
     exit()
 else:
     single_cha: str = input("Enter a single character: ")
-if(len(single_cha) != 1):
-    print("Character must be a single character.")
+if (len(single_cha) != 1):
+    print("Error: Character must be a single character.")
     exit()
 else:
     print(f"Searching for {single_cha} in {five_cha_word}...")
 
 char_amount: int = 0
 
-if(single_cha == five_cha_word[0]):
+if (single_cha == five_cha_word[0]):
     char_amount += 1
     print(single_cha + " found at index 0")
-if(single_cha == five_cha_word[1]):
+if (single_cha == five_cha_word[1]):
     char_amount += 1
     print(single_cha + " found at index 1")
-if(single_cha == five_cha_word[2]):
+if (single_cha == five_cha_word[2]):
     char_amount += 1
     print(single_cha + " found at index 2")
-if(single_cha == five_cha_word[3]):
+if (single_cha == five_cha_word[3]):
     char_amount += 1
     print(single_cha + " found at index 3")
-if(single_cha == five_cha_word[4]):
+if (single_cha == five_cha_word[4]):
     char_amount += 1
     print(single_cha + " found at index 4")
-if(char_amount == 0):
+if (char_amount == 0):
     print(f"No instances of {single_cha} found in {five_cha_word}")
-elif(char_amount == 1):
+elif (char_amount == 1):
     print(f"{char_amount} instance of {single_cha} found in {five_cha_word}")
 else:
     print(f"{char_amount} instances of {single_cha} found in {five_cha_word}")
