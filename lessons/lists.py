74f2297d921418539210c4ad7bdf06043a17b100
Connor Hall <cjhall@unc.edu>
2023-02-22 19:21:03 -0500

    Lists Lesson

---
+++ lessons/lists.py
@@ -0,0 +1,10 @@
+"""Practice with lists."""
+
+grocery_list: list[str] = list()
+grocery_list.append("bananas")
+grocery_list.append("milk")
+grocery_list.append("bread")
+grocery_list[1] = "cereal"
+grocery_list.pop(2)
+grocery_list.append("bananas")
+print(grocery_list)
