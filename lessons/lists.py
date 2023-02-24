d636b1137b6b08615fe1a075ea398b1b4ae7ac24
Connor Hall <cjhall@unc.edu>
2023-02-24 18:49:35 -0500

    Lists Lesson

--- lessons/lists.py
+++ lessons/lists.py
@@ -1,10 +1,10 @@
-"""Practice with lists."""
+def dup(xs: list[str]):
+    start_len: int = len(xs)
+    i: int = 0
+    while i <= start_len - 1:
+        xs.append(xs[1])
+        i += 1
 
-grocery_list: list[str] = list()
-grocery_list.append("bananas")
-grocery_list.append("milk")
-grocery_list.append("bread")
-grocery_list[1] = "cereal"
-grocery_list.pop(2)
-grocery_list.append("bananas")
-print(grocery_list)
+groceries: list[str] = ["apples", "eggs"]
+print(dup(groceries))
+print(groceries)
