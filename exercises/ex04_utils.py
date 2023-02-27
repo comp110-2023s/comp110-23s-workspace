8db0d305c37366d114b7d8919e89a3424046e415
Reem Fayyad <rmfayyad@unc.edu>
2023-02-27 13:47:36 -0500

    final ex04

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,54 @@
+"""EX04 - 'list' Utility Functions!"""
+
+__author__ = "730558596"
+
+
+def all(input: list[int], num: int) -> bool:
+    """Returns true or false statement given the list and the num value."""
+    count: int = 0
+    yes: int = 0
+    if len(input) == yes:
+        return False
+    while count < len(input):
+        if input[count] != num:
+            return False
+        count = count + 1 
+    return True
+ 
+        
+def max(input: list[int]) -> int:
+    """Returns maximum value within the list, but will result in a error if there is a empty list!"""
+    if len(input) == 0: 
+        raise ValueError("max() arg is an empty List")
+    maxint: int = input[0]
+    count: int = 0
+    while count < len(input):
+        if maxint < input[count]: 
+            maxint = input[count]
+            count = count + 1
+        else:
+            count = count + 1
+    return maxint
+
+
+def is_equal(input1: list[int], input2: list[int]) -> bool:
+    """Checks if two strings are matching. Returns true if they are the same list and if the function was given two empty strings. Returns false if the lists are not the same."""
+    if len(input1) != len(input2):
+        return False
+    count: int = 0
+    boolresult: bool = True 
+    while count < len(input1) and len(input2):
+        if input1[count] == input2[count]:
+            boolresult
+        else:
+            return False
+        count = count + 1 
+    return boolresult
+    
+    
+
+
+    
+   
+
+    
---
+++ lessons/debug_practice.py
@@ -0,0 +1,8 @@
+"""Examples for debugger"""
+
+x: list[str]=["hello","world", "!"]
+idx: int = 0
+
+while idx <= len(x)-1:
+    print(x[idx])
+    idx +=1 
