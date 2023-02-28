f8049fce08c2f603b435abda91a4a7751d7ecbc6
Hari Potharaju <hpot@unc.edu>
2023-02-28 00:24:18 -0500

    list excersise

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,38 @@
+def all(list:list[int],x:int) -> bool:
+    """Indicates whether the integer given matches all the ones in the list"""
+    i: int = 0
+    while i < len(list):
+        if list[i] != x:
+            return False
+        else:
+            i += 1
+    return True
+
+def max(list: list[int]) -> int:
+    """Returns the largest number in the list"""
+    if len(list) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0 
+    max_num: int = 0 
+    while i < len(list):
+        if (list[i]) > max_num:
+            max_num = list[i]
+        i += 1
+    return(max_num)
+
+def is_equal(list1:list[int],list2:list[int]) -> bool:
+    """Returns every element in two lists match at every index"""
+    if len(list1) != len(list2):
+        return False
+    i: int = 0
+    while i < len(list1):
+        if list1[i] == list2[i]:
+            i += 1
+        else:
+            return False
+    return True
+
+
+
+
+
