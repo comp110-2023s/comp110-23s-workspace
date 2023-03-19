e6a3807081c0b03fec6f4070abd7bc9be820d31a
Connor Hall <cjhall@unc.edu>
2023-03-19 16:57:31 -0400

    Progress on EX06.

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -2,78 +2,88 @@
 
 __author__ = "730556365"
 
+from random import randint
 
 points: int = 0
 player: str = ""
-
-from random import randint
+num_guesses: int = 0
+secret_num: int = randint(0, 10)
+num_clues: int = 2
 
 def main() -> None:
-    global points
+    global points, num_guesses, secret_num, num_clues
     greet()
-    secret_num: int = randint(0, 10)
-    num_guesses: int = 0
     playing: bool = True
 
     while num_guesses < 3 and playing is True:
-        user_choice: str = input("Would you like to get a clue (type '1'), guess the number (type '2'), or end the game (type '3')?: " )
+        user_choice: str = input("\nWould you like to get a clue (type '1'), guess the number (type '2'), or end the game (type '3')?: " )
         if user_choice == "1":
-            get_clue(secret_num)
+            if num_clues > 0:
+                get_clue(secret_num)
+                num_clues -= 1
+            else:
+                print("\nYou are out of clues. You must attempt to guess the number.")
         elif user_choice == "2":
             num_guesses += 1
-            print(f"Guess #{num_guesses}:")
-            points = guess_num(points, secret_num)
+            print(f"\nGuess #{num_guesses}:")
+            points = guess_num(points)
         elif user_choice == "3":
-            print(f"Game over, {player}. Your final score is {points}. Good luck next time.")
             playing = False
         else:
-            print("Invalid input. Please type 1, 2, or 3.")
+            print("\nInvalid input. Please type 1, 2, or 3.")
+    
+    print(f"\nGame over, {player}. Your final score is {points}. Good luck next time.")
+
 
 
 def greet() -> None:
     global player
-    player = input(f"Welcome to Guess The Number! You will have 3 chances to guess a number from 1 to 10. You can ask for one clue per random number. What is your name?: ")
+    player = input(f"Welcome to Guess The Number! You will have 3 chances to guess a number from 1 to 10. You can ask for two clues per random number.\nWhat is your name?: ")
 
 
-def get_clue(secret_num: int) -> None:
+
+def get_clue(secret: int) -> None:
     global points
     points -= 5
-    clue_choice: str = input("Which type of clue do you want? Type 'even or odd', 'greater than', or 'less than': " )
+    print(f"\nPoint Total: {points}")
+    clue_choice: str = input("\nWhich type of clue do you want? Type 'even or odd', 'greater than', or 'less than': " )
+    while clue_choice != "even or odd" and clue_choice != "greater than" and clue_choice != "less than":
+        clue_choice = input(f"\n{clue_choice} is not a valid input. Please try again: ")
     
     if clue_choice == "even or odd":
-        if secret_num % 2 == 0:
-            print("The number is even.")
+        if secret % 2 == 0:
+            print("\nThe number is even.")
         else:
-            print("The number is odd.")
+            print("\nThe number is odd.")
     elif clue_choice == "greater than":
-        greater_int: int = int(input("Type the number you would like to know if the secret number is greater than: "))
-        if secret_num > greater_int:
-            print(f"The secret number is greater than {greater_int}")
+        greater_int: int = int(input("\nType the number you would like to know if the secret number is greater than: "))
+        if secret > greater_int:
+            print(f"\nThe secret number is greater than {greater_int}")
         else:
-            print(f"The secret number is not greater than {greater_int}")
+            print(f"\nThe secret number is not greater than {greater_int}")
     elif clue_choice == "less than":
-        less_int: int = int(input("Type the number you would like to know if the secret number is greater than: "))
-        if secret_num < less_int:
-            print(f"The secret number is less than {less_int}")
+        less_int: int = int(input("\nType the number you would like to know if the secret number is less than: "))
+        if secret < less_int:
+            print(f"\nThe secret number is less than {less_int}")
         else:
-            print(f"The secret number is not less than {less_int}")
+            print(f"\nThe secret number is not less than {less_int}")
 
 
         
-def guess_num(points: int, secret_num: int) -> int:
-    player_guess = int(input("What do you think the secret number is?: "))
+def guess_num(points: int) -> int:
+    global secret_num, num_guesses, num_clues
+    player_guess = int(input("\nWhat do you think the secret number is?: "))
     
     if player_guess == secret_num:
-        print(f"Correct. The secret number is {secret_num}.")
         points += 50
+        print(f"\nCorrect. The secret number is {secret_num}.\nPoint Total: {points}")
+        num_guesses = 0
+        num_clues = 2
+        secret_num = randint(0, 10)
     else:
-        print(f"Wrong. The secret number is not {player_guess}")
+        print(f"\nWrong. The secret number is not {player_guess}.")
 
     return points
-
-
-
-
 
 
 
