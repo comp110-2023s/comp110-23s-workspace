0b07c4c9900edf44bbfd6e074c294cab0b60304a
hpot7 <122501356+hpot7@users.noreply.github.com>
2023-03-29 22:20:03 -0400

    yay

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -1,36 +1,42 @@
-""""Dictionary Function! - Ex07"""
+"""Dictionary Function - EX07!"""
 
 __author__ = "730575328"
 
-def invert(given:dict[str,str]) -> dict[str,str]:
+
+def invert(given: dict[str, str]) -> dict[str, str]:
     """Returns the inverted version of the given dictionary."""
-    new_dict: dict[str,str] = {}
-    for x in given:
-        y = given[x]
-        new_dict[y] = x
+    new_dict: dict[str, str] = {}
+
+    for key, value in given.items():
+        if value in new_dict:
+            raise KeyError (f"{value} is already in new_dict!")
+        else:
+            new_dict[value] = key
     return new_dict
 
-def favorite_color(given:dict[str,str]) -> str:
+
+def favorite_color(given: dict[str, str]) -> str:
     """Returns the favorite color that was repeated the most."""
     color: list[str] = []
     indx: int = 0
-    temp = 0
+    temp: int = 0
     temp_color: str = ""
-    new_dict: dict[str,str] = {}
     for x in given:
         y = given[x] 
         color.append(y)
     for i in color:
         for x in color:
-            if x == i: indx += 1
+            if x == i: 
+                indx += 1
         if indx > temp:
             temp = indx
             temp_color = i
     return temp_color
 
+
 def count(list1: list[str]) -> dict[str, int]:
     """Takes a list and returns how many times each unique value was repeated in the list in a dictionary."""
-    new_dict: dict[str,int] = {}
+    new_dict: dict[str, int] = {}
     for i in list1:
         if i not in new_dict:
             new_dict[i] = 1
@@ -38,4 +44,4 @@ def count(list1: list[str]) -> dict[str, int]:
             new_dict[i] += 1
     return new_dict
 
-print(count(["fish","shark","shark","orca","orca"]))
+print(favorite_color({"max": "brown","matt": "green","julian": "brown", "jasper": "green"}))
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,54 +1,56 @@
-""""Dictionary Function Test - Ex07"""
+"""Dictionary Function Test - EX07!"""
 
 __author__ = "730575328"
 
-from exercises.ex07.dictionary import invert,favorite_color,count
+from exercises.ex07.dictionary import invert, favorite_color, count
 
 
 def test_invert_use() -> None:
     """Use case test for invert function."""
-    dict_list: dict[str,str] = {"a":"b", "c":"d","e":"f"}
-    assert invert(dict_list) == {"b":"a", "d":"c","f":"e"}
+    dict_list: dict[str, str] = {"a": "b", "c": "d", "e": "f"}
+    assert invert(dict_list) == {"b": "a", "d": "c", "f": "e"}
 
 
 def test_invert_use2() -> None:
     """Second use case test for invert function."""
-    dict_list: dict[str,str] = {"cat":"dog"}
-    assert invert(dict_list) == {"dog":"cat"}
+    dict_list: dict[str, str] = {"cat": "dog"}
+    assert invert(dict_list) == {"dog": "cat"}
 
 
 def test_invert_edge() -> None:
     """Edge test for invert function."""
-    dict_list: dict[str,str] = {}
+    dict_list: dict[str, str] = {}
     assert invert(dict_list) == {}
 
+
 def test_favorite_color_use() -> None:
-    """Use case test for the favorite color function"""
-    dict_list: dict[str,str] = {"max":"brown","matt":"green","julian":"brown"}
+    """Use case test for the favorite color function for a tie."""
+    dict_list: dict[str, str] = {"max": "brown","matt": "green","julian": "brown", "jasper": "green"}
     assert favorite_color(dict_list) == {"brown"}
 
 def test_favorite_color_use2() -> None:
-    """Second use case test for the favorite color function"""
-    dict_list: dict[str,str] = {"maddi":"lavender","lucy":"red","aarya":"blue","jude bellingham":"lavender"}
+    """Second use case test for the favorite color function."""
+    dict_list: dict[str, str] = {"maddi": "lavender","lucy": "red", "aarya": "blue","jude bellingham": "lavender"}
     assert favorite_color(dict_list) == {"lavender"}
 
 def test_favorite_color_edge() -> None:
-    """Edge case test for the favorite color function"""
+    """Edge case test for the favorite color function."""
     dict_list: dict[str,str] = {}
     assert favorite_color(dict_list) == {}
 
+
 def test_count_use() -> None:
-    """Use case test for the count function"""
-    test_list: list[str] = ["apple","apple","orange","apple","grapes"]
+    """Use case test for the count function."""
+    test_list: list[str] = ["apple", "apple", "orange", "apple", "grapes"]
     assert count(test_list) == {'apple': 3, 'orange': 1, 'grapes': 1}
 
 def test_count_use2() -> None:
-    """Second use case test for the count function"""
-    test_list: list[str] = ["fish","shark","shark","orca","orca"]
+    """Second use case test for the count function."""
+    test_list: list[str] = ["fish", "shark", "shark", "orca", "orca"]
     assert count(test_list) == {'fish': 1, 'shark': 2, 'orca': 2}
 
 def test_count_edge() -> None:
-    """Edge case test for the count function"""
+    """Edge case test for the count function."""
     test_list: list[str] = []
     assert count(test_list) == {}
 
