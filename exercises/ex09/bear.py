f087e71110da1cb316ad92459144e3962f05caf9
Connor Hall <cjhall@unc.edu>
2023-04-14 20:35:14 -0400

    EX09.

--- exercises/ex09/bear.py
+++ exercises/ex09/bear.py
@@ -2,7 +2,12 @@
 
 class Bear:
     
+    age: int
+    hunger_score: int
+
     def __init__(self):
+        self.age = 0
+        self.hunger_score = 0
         return None
     
     def one_day(self):
