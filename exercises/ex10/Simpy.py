c0336100857bf3593785b3b3ee32068b1efbc9e4
Hari Potharaju <hpot@unc.edu>
2023-04-23 17:15:00 -0400

    progress

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -4,10 +4,24 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730575328"
 
 
 class Simpy:
+    
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, param: list[float]) -> None:
+        self.values = param
+
+    def __str__(self) -> str:
+        return f'simpy({self.values})'
+    
+    def fill(self, float: float, int: int) -> None:
+        i = 0 
+        while int > i: 
+            self.values.append(float)
+            i += 1
+
+    def arange(self, start: float, stop: float, step: float):
+        
